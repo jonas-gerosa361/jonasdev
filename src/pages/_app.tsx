4989bf1b09b21f2e4a38dae2940efa6c740e5912
JGerosa <JGerosa@its.jnj.com>
2022-03-05 12:23:58 -0300

    Testing twitter:card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { Header } from '../components/Header';
+import Head from "next/head";
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="twitter:card" content="summary"></meta>
+        <meta name="twitter:site" content="@jonas_gerosa"></meta>
+        <meta name="twitter:creator" content="@jonas_gerosa"/>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
