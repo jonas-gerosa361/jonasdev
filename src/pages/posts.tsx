9ce29e01c483901dae764aa1b88cdd3a0c145569
JGerosa <JGerosa@its.jnj.com>
2022-09-12 21:07:07 -0300

    Fixed ssl issue on cms.jonasdev.com.br

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -3,6 +3,7 @@ import { GetStaticProps } from 'next';
 import BlogPosts from "../components/BlogPosts";
 import slugify from "slugify";
 import axios from "axios";
+import * as https from "https";
 
 interface PostProps {
   nid: string,
@@ -33,11 +34,15 @@ export default function Posts({posts}: PostsProps) {
 }
 
 export const getStaticProps: GetStaticProps = async() => {
-  const posts = await axios('https://cms.jonasdev.com.br/api/posts')
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
+  const posts = await axios.get('https://cms.jonasdev.com.br/api/posts', {httpsAgent})
     .then(response => response.data)
     .catch((error) => {
       console.log(error);
-      return {};
+      return [];
     })
 
   const filteredPosts = posts.map((post: PostProps) => {
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import BlogPost from '../../components/BlogPost';
+import * as https from "https";
 
 interface PostProps {
   post: {
@@ -42,8 +43,11 @@ export const getStaticPaths: GetStaticPaths = async() => {
 
 export const getStaticProps: GetStaticProps = async({params}) => {
   const {slug} = params;
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
 
-  const post = await axios(`https://cms.jonasdev.com.br/api/posts/get/${slug}`)
+  const post = await axios(`https://cms.jonasdev.com.br/api/posts/get/${slug}`, {httpsAgent})
     .then(response => response.data[0])
     .catch((error) => {
       console.log(error);
